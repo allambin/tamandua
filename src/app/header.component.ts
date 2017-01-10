0a7101587b25fdb2c1e37f38cc2e55881c29aa38
allambin <annelise.lambin@gmail.com>
2017-01-10 19:34:37 +0100

    add a header and some HTML

---
+++ src/app/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tam-header',
+  templateUrl: './header.component.html',
+  styles: []
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
